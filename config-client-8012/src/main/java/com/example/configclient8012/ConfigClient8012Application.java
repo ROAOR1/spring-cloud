0c2c283d5cbf1df973b3aedda47a0f7f31c63e42
wangwenhao <1402298950@qq.com>
2019-12-19 11:17:54 +0800

    Bus

--- config-client-8012/src/main/java/com/example/configclient8012/ConfigClient8012Application.java
+++ config-client-8012/src/main/java/com/example/configclient8012/ConfigClient8012Application.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @SpringBootApplication
 public class ConfigClient8012Application {
-
     public static void main(String[] args) {
         SpringApplication.run(ConfigClient8012Application.class, args);
     }
-
 }
